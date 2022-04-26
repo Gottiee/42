cd18d8b8ffe604b48bcbab72a1d5d3ba10532875
Gottiee <gottiedev@gmail.com>
2022-04-26 17:55:06 +0000

    add ft atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -10,11 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int	i;
+	int	neg;
+	int nbr;
 
+	i = 0;
+	nbr = 0;
+	neg = 1;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i ++;
-	
+	if (str[i] == '-')
+	{
+		neg *= - 1;
+		i ++;
+	}
+	if (str[i] == '+')
+		i ++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		i ++;
+	}
+	nbr *= neg;
+	return (nbr);
 }
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%d\n", ft_atoi(argv[1]));
+}*/
