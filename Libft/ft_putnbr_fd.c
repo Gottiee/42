546badedba0a006f5a3f26fd67661ac12cdf3c69
Gottiee <gottiedev@gmail.com>
2022-05-03 17:59:06 +0000

    need fix put nbr

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -1,14 +1,29 @@
 #include <unistd.h>
 
-char	*ft_putnbr(int n, int *i)
+void	ft_putnbr(int n, int fd)
 {
-	char	*str;
-	int		i;
+	int	c;
 
-	i = 0;
-	if (n > 10)
-		n /= 10;
-	if (n > 0)
-		ft_putnbr(n, i);
-		
+	c = 0;
+	if (n > 9)
+		ft_putnbr(n / 10, fd);
+	c = n % 10 + '0';
+	write(fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+	ft_putnbr(n, fd);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	int fd = open("test", O_CREAT || O_WRONLY);
+	ft_putnbr_fd(23, fd);
+	close(fd);
 }
