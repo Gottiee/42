9d17e8560d2c16d2cece218b694f26ab26fd57b2
Gottiee <gottiedev@gmail.com>
2022-05-05 16:13:37 +0000

    end lstclear

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -10,11 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_lstclear(t_list **lst, void (del)(void *))
 {
+	t_list *tmp;
 	while(*lst -> next)
 	{
-		del(lst -> content);
-		*lst 	
+		del(*lst -> content);
+		tmp = *lst;
+		*lst = *lst -> next;
+		free(lst);
 	}
 }
