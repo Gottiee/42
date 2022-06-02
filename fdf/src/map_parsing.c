7e44f8b3872f9fbf1325673ea4ab13fa4eb33132
Eliot EDY <eedy@e3r10p20.clusters.42paris.fr>
2022-06-02 15:44:07 +0200

    lets go

--- fdf/src/map_parsing.c
+++ fdf/src/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: eedy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:47:27 by eedy              #+#    #+#             */
-/*   Updated: 2022/06/02 14:54:25 by eedy             ###   ########.fr       */
+/*   Updated: 2022/06/02 15:42:52 by eedy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,28 @@
 void	map_parsing(char *file_name)
 {
 	int		fd;
-	//char	***map;
+	char	***map;
 	int		line_count;
+	char 	*str;
 
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
 		error_center(FD_PRB);
 	line_count = 0;
-	while (get_next_line(fd))	
+	while (str != NULL)	
+	{
+		str = get_next_line(fd);
+		free(str);
 		line_count ++;
-	printf("%d\n", line_count);
+	}
+	map = (char ***)malloc(sizeof(char **) * (line_count + 1));
+	if (!map)
+		exit(0);
+	map[line_count] = '\0';
+	close(fd);
+	fd = open(file_name, O_RDONLY);
+	while (map)
+	{
+		map++ = get_next_line(fd);
+	}
 }
