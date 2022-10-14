74540bf952335882220d69d1fc21465f1be8d97a
Eliot EDY <eedy@e2r7p13.clusters.42paris.fr>
2022-10-14 13:01:55 +0200

    did constructor

--- cpp/module_1/ex02/src/class_account.cpp
+++ cpp/module_1/ex02/src/class_account.cpp
@@ -3,10 +3,21 @@
 # include <iomanip>
 # include <iostream>
 
+int	Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::_nbAccounts = 0;
+
 Account::Account(int inital_deposit)
 {
-	//print la creation du comptes etc..
+	this->_amount = inital_deposit;
+	this->_accountIndex = Account::getNbAccounts();
 	this->_displayTimestamp();
+	std::cout << "index:"<< this->_accountIndex << ";amount:";
+	std::cout << this->_amount << ";created" << std::endl;
+	Account::_nbAccounts ++;
 }
 
 Account::~Account()
@@ -22,8 +33,9 @@ void	Account::displayAccountsInfos(void)
 void	Account::_displayTimestamp(void)
 {
 	time_t	timetoday;
-	std::tm	tm;
+	std::tm	*tm;
 
 	std::time (&timetoday);
-	std::cout << std::asctime(localtime((&timetoday)));
+	tm = std::localtime(&timetoday);
+	std::cout << "[" << tm->tm_year + 1900 << tm->tm_mon << tm->tm_mday << "_" << tm->tm_hour << tm->tm_min << tm->tm_sec << "] ";
 }
