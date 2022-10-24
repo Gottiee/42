161e3c6b08ff083bdc604cd2de3369bc37517bb0
Eliot EDY <eedy@e3r7p10.clusters.42paris.fr>
2022-10-24 15:36:18 +0200

    finish ex04

--- cpp/module_1/ex04/include/main.hpp
+++ cpp/module_1/ex04/include/main.hpp
@@ -2,8 +2,11 @@
 #define MAIN_HPP
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include <cerrno>
+#include <cstdlib>
+#include <cstring>
 
 #endif
--- cpp/module_1/ex04/src/main.cpp
+++ cpp/module_1/ex04/src/main.cpp
@@ -3,14 +3,15 @@
 int open_files(std::ifstream &ifs, std::ofstream &ofs, char **argv)
 {
 	std::string		replace(argv[1]);
+
 	replace += ".replace";
-	std::ifstream	ifs(argv[1]);
+	ifs.open(argv[1]);
 	if (ifs.fail())
 	{
 		std::perror("Error");
 		return (1);
 	}
-	std::ofstream	ofs(replace.c_str());
+	ofs.open(replace.c_str());
 	if (ofs.fail())
 	{
 		std::perror("Error");
@@ -19,22 +20,68 @@ int open_files(std::ifstream &ifs, std::ofstream &ofs, char **argv)
 	return (0);
 }
 
-
-
-int main(int argc, char **argv)
+void	print_info(int argc)
 {
 	if (argc < 4 || argc > 4)
 	{
 		std::cout << "# Create a <filename.replace> wich remplace every occurence of string1 per string2 in <filename> #" << std::endl << std::endl;
 		std::cout << "> ./sed <filename> <string1> <string2>" << std::endl;
-		return (0);
+		exit(1);
 	}
+}
+
+std::string	read_file(std::ifstream &file)
+{
+	std::stringstream	buffer;	
+	std::string			str;
+
+	buffer << file.rdbuf();
+	str = buffer.str();
+	return (str);
+}
+
+std::string	swap_char_str(std::string str, char *init, char *replace)
+{
+	long unsigned int	i = 0;
+	long unsigned int	k = 0;
+	std::string 		out;		
+
+	while (i < str.length())
+	{
+		i = str.find(init, i);
+		if (i == std::string::npos)
+			break;
+		while (k < i)
+		{
+			out += str[k];
+			k ++;
+		}
+		out += replace;
+		k += strlen(init);
+		i += strlen(init);
+	}
+	while (k < str.length())
+	{
+		out += str[k];
+		k ++;
+	}
+	return (out);
+}
+
+int main(int argc, char **argv)
+{
+	print_info(argc);
 
 	std::ifstream	file;
 	std::ofstream	file_replace;
+	std::string		file_in;
+	std::string		file_out;
+
 	if (open_files(file, file_replace, argv) == 1)
 		return (1);
-
-	ifs.close();
-	ofs.close();
+	file_in = read_file(file);
+	file_out = swap_char_str(file_in, argv[2], argv[3]);
+	file_replace << file_out;
+	file.close();
+	file_replace.close();
 }
