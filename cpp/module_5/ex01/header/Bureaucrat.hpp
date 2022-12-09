a4b64fc1d700a06b000e5791fc93b57280a5fabd
Gottiee <gottiedev@gmail.com>
2022-12-09 16:31:38 +0100

    start exo1

---
+++ cpp/module_5/ex01/header/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat
+{
+
+	/*******************/
+	/*      CANON      */
+	/*******************/
+
+	public:
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(Bureaucrat const &src);
+	~Bureaucrat();
+	Bureaucrat	&operator=(Bureaucrat const &src);
+	void		operator++ (int);
+	void		operator-- (int);
+	std::string	getName(void) const;
+	int			getGrade(void) const;
+	class GradeTooHighException : public std::exception
+	{
+		public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+		virtual const char* what() const throw();
+	};
+	
+	private:
+	Bureaucrat();
+	std::string _name;
+	int			_grade;
+
+};
+
+std::ostream	&operator<<(std::ostream& os, Bureaucrat const &src);
+
+#endif
