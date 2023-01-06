b3b4ae10b108e8047062610949682df2a25323f7
Eliot EDY <eedy@e3r12p21.clusters.42paris.fr>
2023-01-06 16:13:35 +0100

    nan /

---
+++ cpp/module_7/ex02/header/Array.hpp
@@ -0,0 +1,16 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template < typename T >
+class Array
+{
+	Public:
+	Array::Array:{
+	T *tab = new T();
+	}
+
+	Private:
+	T *_tab;
+};
+
+#endif
---
+++ cpp/module_7/ex02/src/main.cpp
@@ -0,0 +1,8 @@
+//#include "../header/Array.hpp"
+#include <iostream>
+
+int main(void)
+{
+	int *a = new[] int();
+	std::cout << a << " et " << *a << std::endl;
+}
