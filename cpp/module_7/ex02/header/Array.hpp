65de4812236f6afe6f67d985981bf1e2a82f63c9
Gottiee <gottiedev@gmail.com>
2023-01-07 17:30:40 +0100

    finish module 7

--- cpp/module_7/ex02/header/Array.hpp
+++ cpp/module_7/ex02/header/Array.hpp
@@ -1,16 +1,72 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+#include <string>
+
+class sizePro: public std::exception
+{
+	public:
+	virtual const char* what() const throw()
+	{
+		return ("The array is smaller than the unsigned int you enter");
+	}
+};
+
 template < typename T >
 class Array
 {
-	Public:
-	Array::Array:{
-	T *tab = new T();
+	public:
+
+	Array(): _size(0)
+	{
+		_tab = new T[0];
 	}
 
-	Private:
-	T *_tab;
+	Array(unsigned int n): _size(n)
+	{
+		_tab = new T[n];
+	}
+
+	Array(const Array &src)
+	{
+		_tab = new T[src._size];
+		_size = src._size;
+		for (unsigned int i = 0; i < _size; i ++)
+			_tab[i] = src._tab[i];
+	}
+
+	T &operator=(const Array &src)
+	{
+		delete [] _tab;
+		_tab = new T[src._size];
+		_size = src._size;
+		for (int i = 0; i < _size; i ++)
+			_tab[i] = src._tab[i];		
+		return (*this);
+	}
+
+	T	&operator[](unsigned int i)
+	{
+		if (i >= _size)
+			throw sizePro();
+		return (_tab[i]);
+	}
+
+	unsigned int size() const
+	{
+		return(_size);
+	}
+
+	~Array()
+	{
+		delete [] _tab;
+		_size = 0;
+	}
+
+	private:
+	T 				*_tab;
+	unsigned int	_size;
 };
 
 #endif
--- cpp/module_7/ex02/src/main.cpp
+++ cpp/module_7/ex02/src/main.cpp
@@ -1,8 +1,68 @@
-//#include "../header/Array.hpp"
+#include "../header/Array.hpp"
 #include <iostream>
 
 int main(void)
 {
-	int *a = new[] int();
-	std::cout << a << " et " << *a << std::endl;
+	Array<int> test(4);
+	Array<std::string> test_char(2);
+	Array<float> void_test;
+
+	test[0] = 66;
+	test[1] = 0;
+	test[2] = 14;
+	test[3] = 20;
+
+	test_char[0] = "nice";
+	test_char[1] = "one";
+
+	std::cout << "size of test Array<int>: " << test.size() << std::endl;
+	std::cout << "print of value:\n";
+
+	for (unsigned int i = 0; i < test.size(); i++)
+		std::cout << test[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "\nsize of test_char : "<< test_char.size() << std::endl;
+	std::cout << "print test_char value:\n";
+	std::cout << test_char[0] << " " << test_char[1] << std::endl;
+	std::cout << "\nTesting print void_test[0]\n";
+	try
+	{
+		std::cout << void_test[0] << std::endl;
+	}
+	catch(sizePro &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "\nTesting print test_char[20]\n";
+	try
+	{
+		std::cout << test_char[20] << std::endl;
+	}
+	catch(sizePro &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\nsize of void_test : " << void_test.size() << std::endl;
+
+	std::cout << "\nCopy test\n\n";
+	Array<int> copy(test);
+	for (unsigned int i = 0; i < copy.size(); i++)
+		std::cout << copy[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "\nModified value of copy print it, then print original class to check if it doesn't been changed\n";
+
+	copy[0] = 52;
+	copy[1] = 500;
+	copy[2] = 545;
+	for (unsigned int i = 0; i < copy.size(); i++)
+		std::cout << copy[i] << " ";
+	std::cout << std::endl;
+	
+	for (unsigned int i = 0; i < test.size(); i++)
+		std::cout << test[i] << " ";
+	std::cout << std::endl;
+	return (0);
 }
